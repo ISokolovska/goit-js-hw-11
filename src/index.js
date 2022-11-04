da30561267f2210dcb8094e82d14b7633d0ff77c
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-11-04 11:48:46 +0100

    btnLoadMore

--- src/index.js
+++ src/index.js
@@ -10,21 +10,24 @@ const galleryImage = document.querySelector('.gallery');
 const buttonLoadMore = document.querySelector('.load-more');
 
 let query = '';
+let page = 1;
 
 const handleForm = async e => {
   e.preventDefault();
   let perPage = 40;
   query = e.target.searchQuery.value;
+  page = 1;
   galleryImage.innerHTML = '';
+  buttonLoadMore.classList.add('is-hidden');
   const response = await queryImage(query);
   if (response.totalHits) {
     if (response.totalHits > perPage) {
       buttonLoadMore.classList.remove('is-hidden');
-      console.log(response);
+      // console.log(response);
     }
     renderMarkup(response.hits);
     lightbox.refresh();
-    console.log(response.totalHits);
+    // console.log(response.totalHits);
     Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
   } else {
     Notiflix.Notify.failure(
@@ -49,7 +52,7 @@ const renderMarkup = data => {
       }) => {
         return `<div class="photo-card">
                 <a href="${largeImageURL}">
-                <img src="${webformatURL}" alt="${tags}" width="250" loading="lazy" /></a>
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
                 <div class="info">
                 <p class="info-item">
                 <b>Likes: </b>${likes}
@@ -72,16 +75,18 @@ const renderMarkup = data => {
   galleryImage.innerHTML += markup;
 };
 
-let page = 1;
 buttonLoadMore.addEventListener('click', async () => {
   page += 1;
+  buttonLoadMore.classList.add('is-hidden');
   const response = await queryImage(query, page);
-  console.log(response);
-  if (response.totalHits / response.hits.length < page) {
-    buttonLoadMore.classList.add('is-hidden');
+  let perPage = 40;
+  let totalPages = response.totalHits / perPage;
+  if (page > totalPages) {
     Notiflix.Notify.info(
       'We are sorry, but you have reached the end of search results.'
     );
+  } else {
+    buttonLoadMore.classList.remove('is-hidden');
   }
   renderMarkup(response.hits);
   lightbox.refresh();
